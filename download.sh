c35ca466269eb60ba61b5f51fbfb116f95f760fc
Donglai Xiang <xdl13thu@gmail.com>
2023-09-18 13:33:44 -0400

    update URL

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 cd POF/
-wget posefs1.perception.cs.cmu.edu/mtc/mtc_snapshots.zip
+wget domedb.perception.cs.cmu.edu/data/mtc/mtc_snapshots.zip
 unzip mtc_snapshots.zip
 if [ ! -d snapshots ]; then
     echo "Pretrained model not extracted! Please check your setting."
@@ -8,12 +8,12 @@ else
     echo "Pretrained model successfully downloaded."
 fi
 cd ../FitAdam/model
-wget posefs1.perception.cs.cmu.edu/mtc/adam_blendshapes_348_delta_norm.json
+wget domedb.perception.cs.cmu.edu/data/mtc/adam_blendshapes_348_delta_norm.json
 cd ../include/
-wget posefs1.perception.cs.cmu.edu/mtc/InitializeAdamData.h
+wget domedb.perception.cs.cmu.edu/data/mtc/InitializeAdamData.h
 cd ../../data/
 mkdir example_dance && cd example_dance
-wget posefs1.perception.cs.cmu.edu/mtc/example_dance.mp4
+wget domedb.perception.cs.cmu.edu/data/mtc/example_dance.mp4
 cd ../
 mkdir example_speech && cd example_speech
-wget posefs1.perception.cs.cmu.edu/mtc/example_speech.mp4
+wget domedb.perception.cs.cmu.edu/data/mtc/example_speech.mp4
